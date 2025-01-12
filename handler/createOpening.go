68f34e41fa586a92e429fb97b9ecff707f6fe306
Hadryan Silva <hadryan2001@gmail.com>
2025-01-12 15:35:54 -0300

    feat: add createOpening handler logic

--- handler/createOpening.go
+++ handler/createOpening.go
@@ -1,12 +1,43 @@
 package handler
 
 import (
+	"github.com/HadryanSilva/gopportunities/config"
+	"github.com/HadryanSilva/gopportunities/schemas"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
 func CreateOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusCreated, gin.H{
-		"message": "POST opening",
-	})
+	request := CreateOpeningRequest{}
+
+	ctx.BindJSON(&request)
+
+	if err := request.Validate(); err != nil {
+		logger.Errf("Validation error: %v", err)
+		sendError(ctx, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	opening := schemas.Opening{
+		Role:     request.Role,
+		Company:  request.Company,
+		Location: request.Location,
+		Remote:   *request.Remote,
+		Link:     request.Link,
+		Salary:   request.Salary,
+	}
+
+	if err := db.Create(&opening).Error; err != nil {
+		logger.Errf("Failed to create opening: %v", err)
+		sendError(ctx, http.StatusInternalServerError, "Failed to create opening")
+		return
+	}
+
+	sendSuccess(ctx, "create-opening", opening)
+
+}
+
+func InitializeHandler() {
+	logger = config.GetLogger("handler")
+	db = config.GetPostgres()
 }
--- handler/handler.go
+++ handler/handler.go
@@ -1,1 +1,11 @@
 package handler
+
+import (
+	"github.com/HadryanSilva/gopportunities/config"
+	"gorm.io/gorm"
+)
+
+var (
+	logger *config.Logger
+	db     *gorm.DB
+)
