29332c7508b93b0bc4cd221e60b79d2f88c74dc4
Hadryan Silva <hadryan2001@gmail.com>
2025-01-12 16:41:53 -0300

    feat: add request and response types

--- handler/response.go
+++ handler/response.go
@@ -3,7 +3,6 @@ package handler
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
-	"net/http"
 )
 
 func sendError(ctx *gin.Context, code int, message string) {
@@ -14,9 +13,9 @@ func sendError(ctx *gin.Context, code int, message string) {
 	})
 }
 
-func sendSuccess(ctx *gin.Context, op string, data interface{}) {
+func sendSuccess(ctx *gin.Context, code int, op string, data interface{}) {
 	ctx.Header("Content-Type", "application/json")
-	ctx.JSON(http.StatusCreated, gin.H{
+	ctx.JSON(code, gin.H{
 		"message": fmt.Sprintf("Operation from handler: %s successfully", op),
 		"data":    data,
 	})
